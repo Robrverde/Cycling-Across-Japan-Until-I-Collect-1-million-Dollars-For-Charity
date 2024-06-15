c9805272bc911a21109aa297cf5dd49768ed24ce
Robrverde <robrverde2000@gmail.com>
2024-06-14 22:26:23 -0400

    Fixed parallax of background

--- BG.gd
+++ BG.gd
@@ -1,7 +1,7 @@
 extends ParallaxBackground
 
-var scrollSpeed = 100
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	scroll_offset.x -=scrollSpeed*delta
+	scroll_base_offset -= Vector2(100, 0) * delta
