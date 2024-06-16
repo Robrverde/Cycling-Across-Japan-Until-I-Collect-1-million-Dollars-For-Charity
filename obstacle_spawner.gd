4d0b9ad3e7ea321cce8a65de80a02ee979ce4623
Gbigott <92874470+Gbigott@users.noreply.github.com>
2024-06-16 05:20:13 -0400

    obstacle spawner added
    the obstacle spawner generates randomly the obstacle or power up but, just for the first time then they not appear any more.

--- obstacle_spawner.gd
+++ obstacle_spawner.gd
@@ -1,10 +1,18 @@
 extends Node2D
 
-#@onready var time = $Timer
-#
-#var pete = preload("res://pete_power.tscn")
-#var stopSign = preload("res://stop_sign.tscn")
-#var orange = preload("res://debuff_orange.tscn")
-#
-#func _on_Timer_timeout():
-	#spawn_ob
+@onready var timer = $Timer
+
+var pete = preload("res://pete_power.tscn")
+var stopSign = preload("res://stop_sign.tscn")
+var orange = preload("res://debuff_orange.tscn")
+
+func _ready():
+	randomize()
+
+func _on_Timer_timeout():
+	spawn_obstacle()
+	
+func spawn_obstacle():
+	var obstacle = pete.instance()
+	add_child(obstacle)
+	obstacle.position.y = randi()%400 + 150
